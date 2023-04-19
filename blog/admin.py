06426f4f72aa3b4256978814da5d4bff6a8cc41f
juan <juankrios15@gmail.com>
2023-04-19 11:24:41 -0500

    blog admin

--- blog/admin.py
+++ blog/admin.py
@@ -3,8 +3,33 @@ from django.contrib import admin
 from .models import Tag, GalleryImage, Post, Comment, Like
 
 
-admin.site.register(Tag)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "user", "is_active", "created_time", "updated_time")
+    list_filter = ("user__username", "is_active")
+    search_fields = ("title", "user__username")
+    list_per_page = 50
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_per_page = 100
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("id", "comment_text", "post", "user", "is_accepted", "created_time", "updated_time")
+    list_filter = ("user__username", "is_accepted")
+    search_fields = ("comment_text", "user__username")
+    list_per_page = 100
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ("id", "liked", "user", "content_type", "content", "created_time", "updated_time")
+    list_filter = ("user__username", "liked")
+    search_fields = ("user__username",)
+    list_per_page = 100
+
 admin.site.register(GalleryImage)
-admin.site.register(Post)
-admin.site.register(Comment)
-admin.site.register(Like)
